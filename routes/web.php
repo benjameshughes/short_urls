9aec1b08d33e2ae0dd3d1733d2aa38ef05f979de
Ben Hughes <ben@benjh.com>
2024-01-12 14:15:46 +0000

    Add route for links show with prefix 'link'

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,9 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+Route::get('/{id}', [LinksController::class, 'show'])->name('links.show')->prefix('link');
+
 Route::resource('links', LinksController::class)->middleware(['auth', 'verified'])->only([
     'index', 'create', 'store']);
-
-Route::get('/{id}', [LinksController::class, 'show'])->name('links.show')->prefix('link');
 
 require __DIR__.'/auth.php';
